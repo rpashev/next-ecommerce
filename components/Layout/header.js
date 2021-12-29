2fc93c971ce2a151fe056c780ab15962795ad442
rpashev <rossen1991@gmail.com>
2021-12-29 15:10:28 +0200

    new header combination of bootstrap + scss

--- components/Layout/header.js
+++ components/Layout/header.js
@@ -1,55 +1,46 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
+import Link from "next/link";
+import CartIcon from "../UI/cart-icon";
+import styles from "./header.module.scss";
 
 const Header = (props) => {
   return (
-    <Navbar
-      className="py-3"
-      collapseOnSelect
-      expand="lg"
-      bg="dark"
-      variant="dark"
-      
-    >
-      <Container>
-        <Navbar.Brand href="#home" className="text-success fw-bold">
-          My Shop
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mx-auto">
-            <Nav.Link className="mx-3" href="#">
-              Home
-            </Nav.Link>
-            <Nav.Link className="mx-3" href="/shop">
-              Shop
-            </Nav.Link>
-            <Nav.Link className="mx-3" href="/about">
-              Aboutttt
-            </Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link className="mx-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="currentColor"
-                className="bi bi-cart"
-                viewBox="0 0 16 16"
-              >
-                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-              </svg>
-            </Nav.Link>
-            <Nav.Link className="mx-3" href="/login">
-              Login
-            </Nav.Link>
-            <Nav.Link className="mx-3" href="/sign-up">
-              Sign Up
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <header className={styles.header}>
+      <div className="container h-100">
+        <div className="row align-items-center justify-content-center h-100">
+          <div className={`${styles.logo} col-4 h3`}>My Shop</div>
+          <nav
+            className={`${styles.nav} col-8 row justify-content-between align-items-center`}
+          >
+            <ul className={`${styles["general-nav"]} col-6`}>
+              <li>
+                <Link href="/login">Home</Link>
+              </li>
+              <li>
+                <Link href="/login">Shop</Link>
+              </li>
+              <li>
+                <Link href="/login">About</Link>
+              </li>
+            </ul>
+            <ul className={`${styles["user-nav"]} col-6`}>
+              <li>
+                <Link href="/cart">
+                  <a className={styles["cart-icon"]}>
+                    <CartIcon />
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/login">Login</Link>
+              </li>
+              <li>
+                <Link href="/login">Sign Up</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
   );
 };
 
