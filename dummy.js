40bfc4e7660ba87a3d767fba38b13ab7183234ec
rpashev <rossen1991@gmail.com>
2022-01-03 16:47:56 +0200

    file with some dummy products

---
+++ dummy.js
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    name: "Sports Essentials Logo Gloves",
+    category: "Gloves",
+    price: 25,
+    description:
+      "Carry on with your workout when cold weather arrives. These all-cotton gloves have a polar fleece lining so that wind and cold won't bring your workout to an early end. Ribbed cuffs provide a secure fit.",
+    brand: "Reebok",
+    sizes: ["M", "L"],
+    onSale: true,
+    bestSeller: false,
+    images: ["gloves2-pic1", "gloves2-pic2"],
+  },
+  {
+    name: "COMFORT FLEECE 3.0 GLOVES",
+    category: "Gloves",
+    price: 30,
+    description:
+      "These gloves keep you warm through cold-weather commutes and afternoons on the slope. They have breathable insulation and smart moisture-wicking features so you stay warm and dry. The palm is designed to give you good grip, and there are touchscreen-friendly points on the thumb and forefinger.",
+    brand: "Adidas",
+    sizes: ["S", "M", "L"],
+    onSale: false,
+    bestSeller: false,
+    images: ["gloves1-pic1", "gloves1-pic2"],
+  },
+  {
+    name: "SUPERLITE HAT",
+    category: "Hats & Beanies",
+    price: 24,
+    description:
+      "When the sun's blazing, you've got to dress accordingly. Top off your warm-weather workout ensemble with the adidas Superlite Hat. The comfortable, six-panel construction has perforated side panels to help keep you cool. A non-glare undervisor makes running on bright concrete easy on the eyes.",
+    brand: "Adidas",
+    sizes: ["M"],
+    onSale: false,
+    bestSeller: true,
+    images: ["hat1-pic1", "hat1-pic2"],
+  },
+  {
+    name: "Classics Foundation Bucket Hat",
+    category: "Hats & Beanies",
+    price: 28,
+    description:
+      "Worn by fishermen and farmers since the 1900s, the bucket hat never goes out of style. Wear this low-key look rain or shine to top off your easygoing wardrobe. A contrast Vector logo on the band keeps it sporty.",
+    brand: "Reebok",
+    sizes: ["S", "M", "L"],
+    onSale: true,
+    bestSeller: false,
+    images: ["hat2-pic1", "hat2-pic2"],
+  },
+];
--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,10 +1,33 @@
+import styles from "./index.module.scss";
+import ProductCard from "../../components/products/product-card";
 import Filters from "../../components/shop-page/filters";
 
 const ShopPage = () => {
   return (
-    <div className="container">
-      <div className="row py-5">
+    <div className="container pt-5">
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <a href="/">Home</a>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            Shop
+          </li>
+        </ol>
+      </nav>
+      <hr></hr>
+      <div className="row py-3">
         <Filters />
+        <div className={`${styles["product-list"]} col`}>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+        </div>
       </div>
     </div>
   );
