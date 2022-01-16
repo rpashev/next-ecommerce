959d0fce43f7e861fe6c7669c9e62c4d4a94f404
rpashev <rossen1991@gmail.com>
2022-01-16 13:42:33 +0200

    cart list of items

--- components/cart-page/cart-headers.js
+++ components/cart-page/cart-headers.js
@@ -2,13 +2,13 @@ import styles from "./cart-headers.module.scss";
 
 const CartHeaders = (props) => {
   return (
-    <div className={`row pt-5 justify-content-start ${styles.headers}`}>
-      <h5 className={`col-2`}>Image</h5>
-      <h5 className={`col-3`}>Name</h5>
-      <h5 className={`col-1`}>Price</h5>
-      <h5 className={`col-1`}>Size</h5>
-      <h5 className={`col-2`}>Quantity</h5>
-      <h5 className={`col-2`}>Total Price</h5>
+    <div className="container">
+      <div className={`row pt-5 justify-content-start ${styles.headers}`}>
+        <h6 className={`col-6`}>Product</h6>
+        <h6 className={`col-1`}>Price</h6>
+        <h6 className={`col-3`}>Quantity</h6>
+        <h6 className={`col-1`}>Total Price</h6>
+      </div>
     </div>
   );
 };
--- components/cart-page/cart-item.js
+++ components/cart-page/cart-item.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import styles from "./cart-item.module.scss";
 import { useDispatch } from "react-redux";
 import DeleteIcon from "../UI/delete-icon";
+import { Fragment } from "react";
 
 const CartItem = (props) => {
   const { imgLink, name, price, quantity, size, slug } = props;
@@ -17,27 +18,32 @@ const CartItem = (props) => {
   };
 
   return (
-    <div className={`${styles.item} row justify-content-start align-items-center`}>
-      <div className={`col-2 ps-0`}>
-        <Image
-          src={`/images/products/${imgLink}.jpg`}
-          alt="product"
-          width={150}
-          height={150}
-        />
-      </div>
-      <h5 className={`col-3`}>{name}</h5>
-      <h5 className={`col-1`}>${price}</h5>
-      <h5 className={`col-1`}>{size}</h5>
-      <h5 className={`col-2`}>{quantity}</h5>
-      <h5 className={`col-2`}>${quantity * price}</h5>
-      <DeleteIcon />
-      {/* <button
-        onClick={removeHandler}
-        className={`btn btn-outline-danger shadow-none`}
+    <div className="container">
+      <div
+        className={`${styles.item} row justify-content-start align-items-center`}
       >
-        REMOVE
-      </button> */}
+        <div className={`${styles["main-product"]} col-6`}>
+          <div className={`${styles.img}`}>
+            <Image
+              src={`/images/products/${imgLink}.jpg`}
+              alt="product"
+              width={150}
+              height={150}
+            />
+          </div>
+          <div className={`text-center px-1 mx-auto`}>
+            <h6 className={``}>{name}</h6>
+            <h6 className={`opacity-50 mt-1`}>Size: {size}</h6>
+          </div>
+        </div>
+
+        <h5 className={`col-1`}>${price}</h5>
+        <h5 className={`col-3`}>{quantity}</h5>
+        <h5 className={`col-1`}>${quantity * price}</h5>
+        <DeleteIcon onRemove={removeHandler}/>
+
+        <hr></hr>
+      </div>
     </div>
   );
 };
