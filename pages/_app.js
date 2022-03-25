a7b313b589188ca6d82fd6d4edde7bc9a9f5e82d
rpashev <rossen1991@gmail.com>
2022-03-25 10:15:17 +0200

    test deployment

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import { Provider} from "next-auth/client";
+import { Provider } from "next-auth/client";
 import { Provider as ReduxProvider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import store from "../store";
-import Head from "next/head";
 import Layout from "../components/layout/layout";
+import Head from "next/head";
+
 import "../styles/globals.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
