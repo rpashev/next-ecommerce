dc8e5d5a10f6daedac8f4382d0bba964a688c9e7
rpashev <rossen1991@gmail.com>
2022-04-03 13:32:59 +0300

    fixed scrolling behavior

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,14 @@ import FinalPrompt from "../components/landing-page/final-prompt";
 import HomeIntro from "../components/landing-page/home-intro";
 import Newsletter from "../components/landing-page/newsletter";
 import ProductList from "../components/products/product-list";
-// import axios from "axios";
+
 import { getByField } from "../lib/mongo";
 
 export default function Home(props) {
-  // useEffect(() => {  //pushing all products to mongo
-  //   axios
-  //     .post("/api/products")
-  //     .then((res) => console.log(res))
-  //     .catch((err) => console.log(err));
-  // }, []);
-
   return (
     <Fragment>
       <HomeIntro />
-      <ProductList products={props.products} title="Popular right now"/>
+      <ProductList products={props.products} title="Popular right now" />
       <Newsletter />
       <Features />
       <FinalPrompt />
@@ -33,6 +26,6 @@ export const getStaticProps = async () => {
     props: {
       products: products,
     },
-    revalidate: 3600
+    revalidate: 3600,
   };
 };
