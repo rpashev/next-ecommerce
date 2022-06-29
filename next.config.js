ec5aabb4637216eda7c6013c2df3b81160a9560c
rpashev <rossen1991@gmail.com>
2022-06-29 12:38:42 +0300

    security vulnerability addressed

--- next.config.js
+++ next.config.js
@@ -1,3 +1,30 @@
+const securityHeaders = [
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "same-origin",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "deny",
+  },
+];
+
 module.exports = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
--- pages/api/products.js
+++ pages/api/products.js
@@ -5,7 +5,7 @@
 //   if (req.method === "POST") {
 //     let client;
 //     const connectionString =
-//       "mongodb://rosko_kz:Rossen91kz@cluster0-shard-00-00.cpss2.mongodb.net:27017,cluster0-shard-00-01.cpss2.mongodb.net:27017,cluster0-shard-00-02.cpss2.mongodb.net:27017/ecommerce-app?ssl=true&replicaSet=atlas-dg8hi2-shard-0&authSource=admin&retryWrites=true&w=majority";
+//       process.env.DB_STRING;
 //     try {
 //       client = await MongoClient.connect(connectionString);
 //     } catch (error) {
